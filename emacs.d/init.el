d0dbfd8f4f05a6cd9fd8b35cbe25464f3851e1bf
Rômulo Jales <romulojales@gmail.com>
2025-04-07 16:24:07 +0200

    Adding markdown mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -232,6 +232,14 @@
   :init
   (marginalia-mode))
 
+(use-package markdown-mode
+  :ensure t
+  :straight t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
+
 (use-package emacs
   :ensure nil
   :init
